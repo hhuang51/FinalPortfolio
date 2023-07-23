7159428aa08a57b21364b76209b78e09b3bd1dec
Henley Huang <henthebomb3@gmail.com>
2023-07-23 18:28:32 -0500

    new

---
+++ script.js
@@ -0,0 +1,8 @@
+const mailLink = document.getElementById('mail-link');
+
+mailLink.addEventListener('click', () => {
+
+  // Open mailto link 
+  window.location.href = `mailto:henleyquan2002@gmail.com`;
+
+});
